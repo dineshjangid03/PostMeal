5a6a274e9b2b3cd38f862d1fb741237a9ee2573c
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-11-09 15:11:19 +0530

    init

---
+++ src/main/java/com/postmeal/PostMealApplication.java
@@ -0,0 +1,13 @@
+package com.postmeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postmeal/PostMealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postmeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
