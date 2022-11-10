a846b2d721da26de897ff03f7efe99e364d6b1c4
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-11-10 23:17:48 +0530

    cart repo and service

--- src/main/java/com/postmeal/service/CartService.java
+++ src/main/java/com/postmeal/service/CartService.java
@@ -1,5 +1,24 @@
 package com.postmeal.service;
 
+import com.postmeal.Model.FoodCart;
+import com.postmeal.Model.Item;
+import com.postmeal.exception.FoodCartException;
+import com.postmeal.exception.ItemException;
+
 public interface CartService {
+	
+	public FoodCart saveFoodCart(FoodCart cart);
+	
+	public FoodCart addItemToCart(Integer cart, Integer item)throws FoodCartException, ItemException;
+	
+	public FoodCart viewCartById(Integer cartId);
+	
+	public FoodCart increaseQuantity(Integer cartId, Integer itemId, Integer quantity) throws FoodCartException , ItemException;
+	
+	public FoodCart reduceQuantity(Integer cartId, Integer itemId, Integer quantity) throws FoodCartException , ItemException;
+	
+	public FoodCart removeItem(Integer cartId, Integer itemId) throws FoodCartException ;
+	
+	public FoodCart clearCart(Integer cartId) throws FoodCartException ;
 
 }
