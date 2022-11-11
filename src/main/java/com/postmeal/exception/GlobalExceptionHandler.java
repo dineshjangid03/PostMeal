6b1d5c507ca5db6c4b43bfbe16edda6c144983fc
Pranshu4 <pranshupal4@gmail.com>
2022-11-11 10:26:11 +0530

    Exception

--- src/main/java/com/postmeal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/postmeal/exception/GlobalExceptionHandler.java
@@ -8,13 +8,23 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
 	//ENTITY Exception *****************
 	
-	
+	@ExceptionHandler(ResturantNotFoundException.class)
+	public ResponseEntity<MyErrorDetails> resturantNotFoundException(ResturantNotFoundException e, WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
 	
 	
 	
@@ -29,9 +39,9 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<MyErrorDetails> validationExceptionHandler(MethodArgumentNotValidException mne){
 		MyErrorDetails err = new MyErrorDetails();
-		err.setTimeStamp(LocalDateTime.now());
-		err.setMessage(mne.getMessage());
-		err.setDetails(mne.getBindingResult().getFieldError().getDefaultMessage());
+		err.setLdt(LocalDateTime.now());
+		err.setDesc(mne.getBindingResult().getFieldError().getDefaultMessage());
+		err.setMsg(mne.getMessage());
 		
 		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
 	}
@@ -41,9 +51,9 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<MyErrorDetails> nullPointerExceptionHandler(NullPointerException npe, WebRequest req){
 		MyErrorDetails err = new MyErrorDetails();
-		err.setTimeStamp(LocalDateTime.now());
-		err.setMessage(npe.getMessage());
-		err.setDetails(req.getDescription(false));
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(npe.getMessage());
+		err.setDesc(req.getDescription(false));
 		
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 		
@@ -54,10 +64,23 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<MyErrorDetails> allExceptionHandler(Exception e, WebRequest req){
 		MyErrorDetails err = new MyErrorDetails();
 		
-		err.setTimeStamp(LocalDateTime.now());
-		err.setMessage(e.getMessage());
-		err.setDetails(req.getDescription(false));
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
 		
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<MyErrorDetails> noHandlerFoundException(NoHandlerFoundException e, WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_GATEWAY);
+	}
+	
+	
 }
