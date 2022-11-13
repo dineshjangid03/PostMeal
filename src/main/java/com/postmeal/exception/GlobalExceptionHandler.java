03eee1cc2a03461b6e9e3df8458ebd6e0a2b2c4d
Geetu Rana <geeturana07@gmail.com>
2022-11-13 16:39:49 +0530

    Exception added to gllobal excpetiona handler

--- src/main/java/com/postmeal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/postmeal/exception/GlobalExceptionHandler.java
@@ -15,6 +15,39 @@ public class GlobalExceptionHandler {
 	
 	//ENTITY Exception *****************
 	
+	@ExceptionHandler(ItemException.class)
+	public ResponseEntity<MyErrorDetails> itemExceptionHandler(ItemException e, WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(OrderException.class)
+	public ResponseEntity<MyErrorDetails> orderExceptionHandler(OrderException e,WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(FoodCartException.class)
+	public ResponseEntity<MyErrorDetails> foodCartExceptionHandler(FoodCartException e,WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(e.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+
 	@ExceptionHandler(ResturantNotFoundException.class)
 	public ResponseEntity<MyErrorDetails> resturantNotFoundException(ResturantNotFoundException e, WebRequest req){
 		MyErrorDetails err = new MyErrorDetails();
@@ -59,7 +92,47 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
 	}
 	
+	//handling user current session Exception...
+	
+	@ExceptionHandler(CurrentUserSessException.class)
+	public ResponseEntity<MyErrorDetails> userSessionHandler(CurrentUserSessException cse, WebRequest req){
+		MyErrorDetails err = new MyErrorDetails();
+		err.setLdt(LocalDateTime.now());
+		
+		err.setMsg(cse.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	// Handling Admin current Exception..
+	
+	@ExceptionHandler(CurrentAdminSessException.class)
+	public ResponseEntity<MyErrorDetails> adminSessionHandler(CurrentAdminSessException csr, WebRequest req){
+		
+		MyErrorDetails err = new MyErrorDetails();
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(csr.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+	
+	//Handling Login Exception
+	
+	@ExceptionHandler(LogInException.class)
+	public ResponseEntity<MyErrorDetails> loginExceptionHandler(LogInException le, WebRequest req){
+		
+		MyErrorDetails err = new MyErrorDetails();
+		err.setLdt(LocalDateTime.now());
+		err.setMsg(le.getMessage());
+		err.setDesc(req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDetails>(err, HttpStatus.BAD_REQUEST);
+	}
+	
 	// Handling the validation Exception eg. email password size etc;
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<MyErrorDetails> validationExceptionHandler(MethodArgumentNotValidException mne){
 		MyErrorDetails err = new MyErrorDetails();
@@ -83,7 +156,8 @@ public class GlobalExceptionHandler {
 		
 	}
 	
-	//All Type of Exception Handler 
+	//All Type of Exception Handler.....
+	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<MyErrorDetails> allExceptionHandler(Exception e, WebRequest req){
 		MyErrorDetails err = new MyErrorDetails();
