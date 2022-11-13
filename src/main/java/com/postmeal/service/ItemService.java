eec1e859eef2871820e07fb0c28ea23621bb6f18
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-11-13 15:45:37 +0530

    bug fixed

--- src/main/java/com/postmeal/service/ItemService.java
+++ src/main/java/com/postmeal/service/ItemService.java
@@ -2,6 +2,8 @@ package com.postmeal.service;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.postmeal.Model.Item;
 import com.postmeal.exception.CategoryNotFoundException;
 import com.postmeal.exception.ItemException;
@@ -12,6 +14,8 @@ public interface ItemService {
 	public Item addItem(Item item);
 	
 	public Item updateItem(Item item)throws ItemException;
+	
+	public Item addItemInRestaurant(Integer itemId,@PathVariable("resId") Integer resId) throws ItemException,ResturantNotFoundException;
 	
 	public Item viewItem(Integer itemId)throws ItemException;
 	
